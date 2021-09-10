f5889c220f8da29e6ebd86675167b3ba5d741f6d
jim-conner <jamesdavidconner@gmail.com>
2021-09-10 12:05:20 -0500

    exercise 16

---
+++ tracks_no_id.sql
@@ -0,0 +1,8 @@
+--16.	tracks_no_id.sql: Provide a query that shows all the Tracks, but displays no IDs. The result should include the Album name, Media type and Genre.
+--use a temp table tk.name, 
+select t.Name, g.Name, m.Name
+from Track t
+join Genre g on t.GenreId = g.GenreId
+join MediaType m on t.MediaTypeId = m.MediaTypeId
+
+--17. invoices_line_item_count.sql: Provide a query that shows all Invoices but includes the # of invoice line items.
